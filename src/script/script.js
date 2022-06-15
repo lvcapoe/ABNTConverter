37414ff0340a2071e0ded27711f2345b13f9a1c5
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-15 18:12:40 -0300

    FormatDate function and two new objects

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,8 @@
-const content = document.querySelector('#content');
+function FormatDate(date){
+	return `${date.substring(8,10)}/${date.substring(5,7)}/${date.substring(0,4)}`;
+}
 
+const content = document.querySelector('#content');
 const WebMatter = {
 	GetInfo: function (){
 		return {
@@ -9,13 +12,52 @@ const WebMatter = {
 			title: document.querySelector('#title').value,
 			site: document.querySelector('#siteName').value,
 			year: document.querySelector('#year').value,
+			access: FormatDate(document.querySelector('#access').value)
+		}
+	},
+	WriteInfo: function (info){
+		return( 
+			`
+					${info.lastName.toUpperCase()}, ${info.name}. ${info.title}. ${info.site}, ${info.year}. Disponível em: &lt;${info.link}&gt;. Acesso em: ${info.access}.
+			`
+		);
+	}
+}
+const HomePage = {
+	GetInfo: function (){
+		return {
+			name: document.querySelector('#name').value, 
+			site: document.querySelector('#siteName').value,
+			year: document.querySelector('#year').value,
+			resume: document.querySelector('#resume').value,
+			link: document.querySelector('#url').value,
 			access: document.querySelector('#access').value
 		}
 	},
 	WriteInfo: function (info){
 		return( 
 			`
-					${info.lastName.toUpperCase()}, ${info.name}. ${info.title}. ${info.site}, ${info.year}. Disponível em: &lt;${info.link}&gt;. Acesso em: ${info.access.substring(8,10)}/${info.access.substring(5,7)}/${info.access.substring(0,4)}.
+				${info.name}. ${info.site}, ${info.year}. ${info.resume}. Disponível em: &lt;${info.link}&gt;. Acesso em: ${info.access.substring(8,10)}/${info.access.substring(5,7)}/${info.access.substring(0,4)}.
+			`
+		);
+	}
+}
+const YoutubeVideo = {
+	GetInfo: function (){
+		return {
+			name: document.querySelector('#name').value, 
+			lastName: document.querySelector('#lastName').value,
+			title: document.querySelector('#title').value,
+			site: "Youtube",
+			date: FormatDate(document.querySelector('#date').value),
+			link: document.querySelector('#url').value,
+			access: FormatDate(document.querySelector('#access').value)
+		}
+	},
+	WriteInfo: function (info){
+		return( 
+			`
+					${info.lastName.toUpperCase()}, ${info.name}. ${info.title}. ${info.site}, ${info.date}. Disponível em: &lt;${info.link}&gt;. Acesso em: ${info.access}.
 			`
 		);
 	}
