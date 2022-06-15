a434a9faa6ee7f1836dff89e70262c1add554e5a
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-15 19:42:05 -0300

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,96 @@
+
 function FormatDate(date){
 	return `${date.substring(8,10)}/${date.substring(5,7)}/${date.substring(0,4)}`;
 }
 
+const options = document.querySelector('#options');
 const content = document.querySelector('#content');
+const inputs = document.querySelectorAll('.inputContainer')[0];
+
+options.addEventListener('change', ()=>{
+	let option = options.value;
+	switch(option){
+		case 'Materia':
+			inputs.innerHTML = `
+				<div class="box">
+                        <input id="url" type="text" placeholder="Link URL">
+                    </div>    
+                    <div class="box">
+                        <input id="name" type="text" placeholder="Nome">
+                    </div>
+                    <div class="box">
+                        <input id="lastName" type="text" placeholder="Sobrenome">
+                    </div>
+                    <div class="box">
+                        <input id="title" type="text" placeholder="Título da Matéria">
+                    </div>
+                    <div class="box">
+                        <input id="siteName" type="text" placeholder="Nome do site">
+                    </div>
+                    <div class="box">
+                        <input id="year" type="number" placeholder="Ano">
+                    </div>
+                    <div class="box">
+                        <input id="access" type="date" placeholder="Data de acesso">
+                    </div>
+
+			`;
+		break;
+
+		case 'Youtube':
+			inputs.innerHTML = `
+				<div class="box">
+                        <input id="url" type="text" placeholder="Link URL">
+                    </div>    
+                    <div class="box">
+                        <input id="name" type="text" placeholder="Nome do autor">
+                    </div>
+                    <div class="box">
+                        <input id="lastName" type="text" placeholder="Sobrenome do autor">
+                    </div>
+                    <div class="box">
+                        <input id="title" type="text" placeholder="Título do vídeo">
+                    </div>
+                    <div class="box">
+                        <input id="siteName" type="text" placeholder="Nome do site" readonly value="Youtube">
+                    </div>
+                    <div class="box">
+                        <label>Data do vídeo:</label>
+                        <input id="date" type="date" placeholder="Ano">
+                    </div>
+                    <div class="box">
+                        <label>Data de acesso:</label>
+                        <input id="access" type="date" placeholder="Data de acesso">
+                    </div>
+
+			`;
+		break;
+
+		case 'Homepage':
+			inputs.innerHTML = `
+				<div class="box">
+                        <input id="url" type="text" placeholder="Link URL">
+                    </div>    
+                    <div class="box">
+                        <input id="name" type="text" placeholder="Autor ou Organização">
+                    </div>
+                    <div class="box">
+                        <input id="resume" type="text" placeholder="Emenda">
+                    </div>
+                    <div class="box">
+                        <input id="siteName" type="text" placeholder="Nome do site">
+                    </div>
+                    <div class="box">
+                        <input id="year" type="number" placeholder="Ano">
+                    </div>
+                    <div class="box">
+                        <input id="access" type="date" placeholder="Data de acesso">
+                </div>
+			`;
+		break;
+	}
+});
+
 const WebMatter = {
 	GetInfo: function (){
 		return {
@@ -23,10 +111,11 @@ const WebMatter = {
 		);
 	}
 }
+
 const HomePage = {
 	GetInfo: function (){
 		return {
-			name: document.querySelector('#name').value, 
+			name: document.querySelector('#name').value,
 			site: document.querySelector('#siteName').value,
 			year: document.querySelector('#year').value,
 			resume: document.querySelector('#resume').value,
@@ -42,16 +131,17 @@ const HomePage = {
 		);
 	}
 }
+
 const YoutubeVideo = {
 	GetInfo: function (){
 		return {
 			name: document.querySelector('#name').value, 
-			lastName: document.querySelector('#lastName').value,
-			title: document.querySelector('#title').value,
-			site: "Youtube",
-			date: FormatDate(document.querySelector('#date').value),
-			link: document.querySelector('#url').value,
-			access: FormatDate(document.querySelector('#access').value)
+			lastName: document.querySelector('#lastName').value, 
+			title: document.querySelector('#title').value, 
+			site: "Youtube", 
+			date: FormatDate(document.querySelector('#date').value), 
+			link: document.querySelector('#url').value, 
+			access: FormatDate(document.querySelector('#access').value) 
 		}
 	},
 	WriteInfo: function (info){
@@ -64,5 +154,11 @@ const YoutubeVideo = {
 }
 
 document.querySelector('#abnButton').addEventListener('click', ()=>{
-	content.innerHTML = WebMatter.WriteInfo(WebMatter.GetInfo());
+	if(options.value=='Homepage'){
+		content.innerHTML = HomePage.WriteInfo(HomePage.GetInfo());
+	}else if(options.value=='Youtube'){
+		content.innerHTML = YoutubeVideo.WriteInfo(YoutubeVideo.GetInfo());
+	}else{
+		content.innerHTML = WebMatter.WriteInfo(WebMatter.GetInfo());	
+	}
 });
