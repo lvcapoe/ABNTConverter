93a3a935952a325e3ffa7bed5a7eb59edb8d2367
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-15 17:38:56 -0300

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,19 +1,26 @@
 const content = document.querySelector('#content');
 
-document.querySelector('#abnButton').addEventListener('click', ()=>{
-	let info = {
-		name: document.querySelector('#name').value, 
-		lastName: document.querySelector('#lastName').value,
-		link: document.querySelector('#url').value,
-		title: document.querySelector('#title').value,
-		site: document.querySelector('#siteName').value,
-		year: document.querySelector('#year').value,
-		access: document.querySelector('#access').value
-	};
-	content.innerHTML = `
-		<p>
-			${info.lastName.toUpperCase()}, ${info.name}. ${info.title}. ${info.site}, ${info.year}. Disponível em: &lt;${info.link}&gt;. Acesso em: ${info.access.substring(8,10)}/${info.access.substring(5,7)}/${info.access.substring(0,4)}.
-		cess
+const WebMatter = {
+	GetInfo: function (){
+		return {
+			name: document.querySelector('#name').value, 
+			lastName: document.querySelector('#lastName').value,
+			link: document.querySelector('#url').value,
+			title: document.querySelector('#title').value,
+			site: document.querySelector('#siteName').value,
+			year: document.querySelector('#year').value,
+			access: document.querySelector('#access').value
+		}
+	},
+	WriteInfo: function (info){
+		return( 
+			`
+					${info.lastName.toUpperCase()}, ${info.name}. ${info.title}. ${info.site}, ${info.year}. Disponível em: &lt;${info.link}&gt;. Acesso em: ${info.access.substring(8,10)}/${info.access.substring(5,7)}/${info.access.substring(0,4)}.
+			`
+		);
+	}
+}
 
-	`
+document.querySelector('#abnButton').addEventListener('click', ()=>{
+	content.innerHTML = WebMatter.WriteInfo(WebMatter.GetInfo());
 });
